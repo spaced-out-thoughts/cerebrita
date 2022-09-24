d1f56fd4684ac3ef1a7cb320f2f749988bd66f5a
rob <rdurst495@gmail.com>
2022-09-23 21:03:21 -0400

    add a module

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![no_std]
+
+pub struct Cabezita {
+    tick: u16,
+    tick_limit: u16,
+    iteration: u16,
+}
+
+impl Cabezita {
+    pub fn new() -> Cabezita {
+        Cabezita {
+            tick: 0,
+            tick_limit: 5_000, 
+            iteration: 100,
+        }
+    }
+
+    pub fn increment(&mut self) -> u16 {
+        self.tick = (self.tick + self.iteration) % self.tick_limit;
+
+        self.tick
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,18 @@
 #![no_main]
 
 use panic_halt as _;
+use cabezita::Cabezita;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
     let mut led = pins.d13.into_output();
+    let mut cabezita = Cabezita::new();
 
     loop {
         led.toggle();
-        arduino_hal::delay_ms(100);
+        arduino_hal::delay_ms(cabezita.increment());
     }
 }
